451793fea5c6ecb9bb707effab4ddb2ad1e6d5a7
m <mm@ms-MacBook-Pro.local>
2018-10-25 09:01:47 -0300

    Added practica 6

--- misc/bayes_exp_gamma.py
+++ misc/bayes_exp_gamma.py
@@ -2,7 +2,7 @@
 distributed variable <T>
 """
 import numpy as np
-from scipy.stats import gamma
+from scipy.stats import gamma, expon
 import pandas as pd
 import matplotlib.pyplot as plt
 plt.close('all')
@@ -11,16 +11,21 @@ n = 100
 l = 4
 scale = 1 / l
 a = 2 # alpha
-rv_priori = gamma(a=a, scale=scale)
+rv_priori = expon()
 
 sample = [.75]
 scale_posteriori= 1 / sum(sample)
 rv_posteriori = gamma(a=2* len(sample) + 1, scale=scale_posteriori)
 
-x = np.linspace(rv_priori.ppf(0.001), 30, 100)
+x = np.linspace(rv_priori.ppf(0.0001), 20, 100)
 
 # Figures
 fig, (ax1, ax2) = plt.subplots(1, 2)
-ax1.plot(x, rv_priori.pdf(x), 'r', label='a priori')
+ax1.plot(x, rv_priori.pdf(x), 'r', label='dist.')
+ax1.axvline(x=rv_priori.expect(), color='g', linestyle=':', label='exp. val.')
+ax1.set_title("priori")
+ax1.legend()
 ax2.plot(x, rv_posteriori.pdf(x), 'b',  label='a posteriori')
-
+ax2.axvline(x=rv_posteriori.expect(), color='g', linestyle=':', label='exp. val.')
+ax2.set_title("posteriori")
+ax2.legend()
---
+++ practica_6.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 25 08:27:08 2018
+
+@author: mm
+"""
+import numpy as np
+from scipy import stats as st
+
+
+# Ejercicio 6
+sample = [1.52, 1.65, 1.72, 1.65, 1.72, 1.83, 1.62, 1.75, 1.72, 1.68, 1.51, 
+          1.65, 1.58, 1.65, 1.61, 1.70, 1.60, 1.73, 1.61, 1.52, 1.81, 1.72, 
+          1.50, 1.82, 1.65]
+sample_mean = np.mean(sample)
+
+s2 = sum([(x - sample_mean)**2 for x in sample]) / (len(sample) - 1)
+s = np.sqrt(s2)
+
+t = {
+     '.95': 2.0639
+ }
+interval95= [sample_mean - t['.95'] * s / np.sqrt(len(sample)), 
+             sample_mean + t['.95'] * s / np.sqrt(len(sample))]
+
+# Check the correctness
+st.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=st.sem(sample))
